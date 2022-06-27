20bd0d85e9236167c1b570701c4acc99ec7e8f29
Ryan <highrevz@hotmail.co.uk>
2022-06-27 20:43:04 +0100

    added sound to first key component. sound only plays when correct keydown is pressed.

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -1,10 +1,23 @@
 import "./Key.css";
+import audio from "../Sounds/c3.mp3";
 
 const Key = () => {
-  const handleClick = (e) => {
-    console.log("clicked");
+  const playAudio = (e) => {
+    if (e.key === "a"){
+
+    
+    
+    const audioEl = document.getElementsByClassName("audio-element")[0];
+    audioEl.play();
+    }
   };
-  return <div className="key" onClick={handleClick}></div>;
+  return (
+    <div className="key-white" tabIndex="0" onKeyDown={playAudio}>
+      <audio className="audio-element">
+        <source src={audio}></source>
+      </audio>
+    </div>
+  );
 };
 
 export default Key;
